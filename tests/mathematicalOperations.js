e830cdca014f811184dd4d392fe793bfa75e2468
nikkapallado <nikka.stacktrek@gmail.com>
2023-02-23 21:19:40 +0800

    commit-all

---
+++ tests/mathematicalOperations.js
@@ -0,0 +1,51 @@
+var assert = require(`assert`);
+
+describe(`Test Suite - Mathematical Operations`, function(){
+    // this.beforeEach(function(done){
+    //     // this.timeout(500)
+    //     // setTimeout(done,3000)
+    // })
+
+    it(`Addition of two numbers`, function(){
+        // this.timeout(500)
+        // setTimeout(done, 3000);
+
+        var a = 10;
+        var b = 10;
+
+        var c  = a + b;
+
+        assert.equal(c,20);
+    })
+
+    it(`Subtraction of two numbers`, function(){
+        var a = 10;
+        var b = 10;
+
+        var c  = a - b;
+        
+        assert.equal(c,0);
+    })
+    
+    it(`Multiplication of two numbers`, function(){
+        var a = 10;
+        var b = 10;
+
+        var c  = a * b;
+        
+        assert.equal(c,100);
+    })
+
+    
+    it(`Division of two numbers`, function(){
+        var a = 10;
+        var b = 10;
+
+        var c  = a / b;
+        
+        assert.equal(c,1);
+    })
+
+    
+    it(`Pending state test`)
+})
